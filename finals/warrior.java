6ad3705c544a5bffc1d9a745bd8f35c53dc376a3
velasco,nathanael V. <110461125+natnat0509@users.noreply.github.com>
2025-10-25 10:32:07 +0800

    Update warrior.java

--- finals/warrior.java
+++ finals/warrior.java
@@ -15,21 +15,3 @@ class Warrior extends Character {
         }
     }
 }
-
-// Example subclass 2: Mage
-class Mage extends Character {
-    Mage(String name) {
-        super(name, 90, 120, 60);
-    }
-
-    void fireball(Character enemy) {
-        if (mp >= 25) {
-            mp -= 25;
-            int damage = 20;
-            enemy.hp -= damage;
-            System.out.println(name + " casts Fireball! Deals " + damage + " magic damage.");
-        } else {
-            System.out.println("Not enough MP!");
-        }
-    }
-}
